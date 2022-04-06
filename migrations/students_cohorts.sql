a2d9272cb34ac12174c0bc4c3e74c5bc817313cb
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-04-06 02:42:58 +0000

    update documentation

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -15,3 +15,23 @@ CREATE TABLE students (
   end_date DATE, 
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
 );
+
+--  Table "public.students"
+--    Column   |          Type          |                       Modifiers
+
+-- ------------+------------------------+-------------------------------------------------------
+--  id         | integer                | not null default nextval('students_id_seq'::regclass)
+--  name       | character varying(255) | not null
+--  email      | character varying(255) |
+--  phone      | character varying(32)  |
+--  github     | character varying(255) |
+--  start_date | date                   |
+--  end_date   | date                   |
+--  cohort_id  | integer                |
+-- Indexes:
+--     "students_pkey" PRIMARY KEY, btree (id)
+-- Foreign-key constraints:
+--     "students_cohort_id_fkey" FOREIGN KEY (cohort_id) REFERENCES cohorts(id) ON DELETE CASCADE
+-- Referenced by:
+--     TABLE "assignment_submissions" CONSTRAINT "assignment_submissions_student_id_fkey" FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE
+--     TABLE "assistance_requests" CONSTRAINT "assistance_requests_student_id_fkey" FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE
