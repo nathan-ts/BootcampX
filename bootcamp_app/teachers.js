6c00b42e5cd99f2766b7d08a2da661d54bf7fe62
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-04-07 03:18:38 +0000

    remove sql injection attack vector

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,17 +10,23 @@ const serverConfig = {
 
 const pool = new Pool(serverConfig);
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, 
   cohorts.name AS cohort, 
   COUNT(assistance_requests) AS total_assistances
 FROM teachers JOIN assistance_requests ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teacher, cohort
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`);
